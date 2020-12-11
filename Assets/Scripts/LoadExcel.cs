8271a9d1db0db876759ea4b8a9f5e61465f6369c
hoshino00g <hoshiro1998@icloud.com>
2020-12-11 17:42:31 +0900

    1211

--- Assets/Scripts/LoadExcel.cs
+++ Assets/Scripts/LoadExcel.cs
@@ -16,14 +16,38 @@ public class LoadExcel : MonoBehaviour
     string csvFileName = "【VAS2021】出展者一覧と入稿ファイル名1124";
     string listname = "boothlist";
     string item1 = "";
+    public Text result_txt;
+    public Text output_path;
+    StreamWriter writer;
+    string path;
+    string path_demo = @"C:\Users\HoshinoHiroto\Desktop\aaa.csv";
 
 
 	public void LoadJSON(){
+        Debug.Log("LoadJSON");
         int k = 0;
         ///csv = Resources.Load (OpenFile.path) as TextAsset;
-        csv = File.ReadAllText (OpenFile.path);
-        print(OpenFile.path);
-        print(csv);
+        //try{
+        Debug.Log("ReadAllTest1");
+        Debug.Log(path_demo);
+        Debug.Log(@"" + OpenFile.path);
+        Debug.Log(string.Format(@"{0}", OpenFile.path));
+        #if UNITY_EDITOR
+        csv = File.ReadAllText(string.Format(@"{0}", OpenFile.path));
+        #endif
+
+        #if !UNITY_EDITOR && UNITY_STANDALONE_WIN
+        csv = File.ReadAllText(string.Format(@"{0}", OpenFile.path));
+        //csv = File.ReadAllText(@""+ OpenFile.path);
+        #endif
+        Debug.Log("ReadAllTest2");
+        Debug.Log(csv);
+        // } catch(System.ArgumentException e){
+        // result_txt.fontSize = 8;
+        // result_txt.text = e.ToString();
+        // output_path.text = OpenFile.path;
+        // }
+        Debug.Log("ReadAllTest3");
 		reader = new StringReader (csv);
 		csvDatas = new List<string[]> ();
         JsonObject jsonObject = new JsonObject();
@@ -47,7 +71,7 @@ public class LoadExcel : MonoBehaviour
                 boothitem.boothNameKana = csvDatas[lineCount2 ][2];
                 boothitem.boothDisplayName = csvDatas[lineCount2 ][3];
                 boothitem.boothDisplayNameKana = csvDatas[lineCount2 ][4];
-                boothitem.boothSpawnName = "boothSpawnName" + csvDatas[lineCount2 ][0];
+                boothitem.boothSpawnName = csvDatas[lineCount2 ][0]  + "_SpawnPoint";
                 if(lineCount2 != 0){
                 jsonObject.boothlist[lineCount2 - 1] = boothitem;
                 lineCount2++;
@@ -58,12 +82,35 @@ public class LoadExcel : MonoBehaviour
         }
         string serialisedItemJson = JsonUtility.ToJson(jsonObject);
         print(serialisedItemJson);
+        try{
         string filePath = "PlayerDataInstance.json";
-        var path =  Application.dataPath + "/" + filePath;
+        path =  Application.persistentDataPath + "/" + filePath;
         print(path);
-        var writer = new StreamWriter (path, true); // 上書き
+        writer = new StreamWriter (path, true); // 上書き
         writer.WriteLine (serialisedItemJson);
         writer.Flush ();
         writer.Close ();
+        }
+        catch(System.ArgumentException e){
+        result_txt.text = e.ToString();
+        output_path.text = path;
+        }
+      
+        result_txt.text = "Succeed!!";
+        output_path.text = path;
+     
+
     }
+
+    private static string ToLiteral(string input)
+{
+    using (var writer = new StringWriter())
+    {
+        using (var provider = System.CodeDom.Compiler.CodeDomProvider.CreateProvider("CSharp"))
+        {
+            provider.GenerateCodeFromExpression(new System.CodeDom.CodePrimitiveExpression(input), writer, null);
+            return writer.ToString();
+        }
+    }
+}
 }
--- Assets/Scripts/OpenFile.cs
+++ Assets/Scripts/OpenFile.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 using SFB;
 
 public class OpenFile : MonoBehaviour
 {
     public static string path;
+    public Text filepath;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +32,20 @@ public class OpenFile : MonoBehaviour
         var extensions = new [] {
         new ExtensionFilter("Open CSV", "csv")
         };
-        path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true);
+        WriteResult(StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true));
         #endif
+        filepath.text = path;
     }
+
+    public void WriteResult(string[] paths) {
+        if (paths.Length == 0) {
+            return;
+        }
+
+        path = "";
+        foreach (var p in paths) {
+            path += p + "\n";
+        }
+    }
+    
 }
